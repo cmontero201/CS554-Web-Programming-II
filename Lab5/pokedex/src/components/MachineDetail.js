ec639d951cf9aac642e74877b14aedc0e31ba120
Christian Montero <cmontero@bu.edu>
2020-04-02 07:32:21 -0400

    update lab5

---
+++ Lab5/pokedex/src/components/MachineDetail.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
+import axios from 'axios';
+import noImage from '../img/None.jpeg'
+import NotFound from './NotFound'
+
+const MachineDetail = (props) => {
+
+};
+
+export default MachineDetail;
+// const PokemonDetail = (props) => {
+//     const [ pokeData, setPokeData ] = useState(undefined);
+
+//     let name = null;
+//     let img = null;
+//     let height = null;
+//     let weight = null;
+//     let typeDiv = [];
+//     let abilities = [];
+//     let moves = [];
+//     let history = useHistory();
+
+//     useEffect( 
+//         () => {
+//             async function fetchList() {
+//                 try {
+//                     // Get Pokemon Details with id
+//                     const data = await axios.get('https://pokeapi.co/api/v2/pokemon/' + props.match.params.id);
+//                     setPokeData(data);
+
+//                 } catch (err) {
+//                     setPokeData(null);
+//                     console.log(err);
+//                 };
+//             };
+
+//             fetchList();
+//         }, [ props.match.params.id ]
+//     );
+    
+//     if (pokeData) {
+//         let data = pokeData.data;
+
+//         // Set Image
+//         if (data && data.sprites.front_default) {
+//             img = <img alt = {data.name + "Image"} src = {data.sprites.front_default} />
+//         } else {
+//             img = <img alt = {data.name + "Image"} src = {noImage} />
+//         };
+
+//         name = data.name;
+//         height = data.height * 3.93701 ;                  // convert to inches
+//         weight = (data.weight * 0.220462).toFixed(1);    // convert to lbs with 1 decimal pt
+
+//         typeDiv = data.types && data.types.map( (type) => {
+//             return (
+//                 <Col key = {type.type.name}>
+//                     {type.type.name}
+//                 </Col>   
+//             );
+//         });
+
+//         abilities = data.abilities && data.abilities.map( (ability) => {
+//             return (
+//                 <Row key = {ability.ability.name} className = 'detailsContent'> 
+//                     {ability.ability.name}
+//                 </Row>
+//             );
+//         });
+
+//         moves = data.moves && data.moves.map( (move) => {
+//             return (
+//                 <Row key = {move.move.name} className = 'detailsContent'>
+//                     <p> {move.move.name} </p>
+//                 </Row>
+//             );
+//         });        
+//     }; 
+
+//     // NotFound Component if no PokeData
+//     if (pokeData === null) {
+//         return (
+//             <NotFound/>
+//         );
+//     };
+
+//     return (
+//         <Container className = 'pokeDetails'>
+//             <br />
+//             <Button type="button" id = 'button' onClick={() => history.goBack()}>
+//                 Go back
+//             </Button>
+//             <br />
+//             <br />
+//             <Row className = 'imageHeader'>
+//                 {img}
+//             </Row>
+//             <br />
+//             <Row className = 'detailHead'>
+//                 <h1> {name} </h1>
+//             </Row>
+//             <br />
+//             <Row xs = {1} sm = {1} md = {2} lg = {4} className = 'detailsContent'>
+//                 <Col>
+//                     <h2 className = 'subhead'> Height: </h2>{Math.floor(height / 12) + "' " + Math.ceil(height % 12) + '"'}
+//                 </Col>
+//                 <Col>
+//                     <h2 className = 'subhead'> Weight </h2> {(weight)} lbs
+//                 </Col>
+
+//                 <Col>
+//                     <h2 className = 'subhead'> Type </h2>
+//                     <Col className = 'detailsContent'>
+//                         {typeDiv}
+//                     </Col>
+//                 </Col>
+
+//                 <Col>
+//                     <h2 className = 'subhead'> Abilites </h2>
+//                     <Col className = 'detailsContent'>
+//                         {abilities}  
+//                     </Col>
+                    
+//                 </Col>
+//             </Row>
+//             <br />
+//             <Row >
+//                 <Col className = 'detailsContent'>
+//                     <h2 className = 'subhead'> Moves </h2>
+//                     <br />
+//                     <Row id = 'moves' xs = {1} sm = {3} md = {4} lg = {6}>
+//                         {moves}
+//                     </Row>   
+//                 </Col>
+//             </Row>
+//         </Container>
+//     );
+// };
+
+// export default PokemonDetail;
--- Lab5/pokedex/src/components/Machines.js
+++ Lab5/pokedex/src/components/Machines.js
@@ -1,13 +1,159 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
 import axios from 'axios';
+import noImage from '../img/None.jpeg'
+import Page from './Page'
+import NotFound from './NotFound'
 
-const Machines = () => {
+const Machines = (props) => {
+    const [ machineData, setMachineData ] = useState(undefined);
+    const [ pageData, setPageData ] = useState({});
+    const [ getPage, setGetPage ] = useState(0);
+
+    let currPage = null;
+    let desc = null;
+    let col = null;
+    let img = null;
+    let page = null;
+    let details = {};
+    let info = [];
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    };
+
+    useEffect( 
+        () => {
+            currPage = Number(props.match.params.page);
+            
+            async function fetchList() {
+                try {
+                    setGetPage(currPage);
+                    
+                    // Get currPage Machine Data
+                    const { data } = await axios.get("https://pokeapi.co/api/v2/machine/?limit=20&offset=" + (currPage * 20).toString());
+
+                    if (data.results) {
+                        await data.results.map( async (machine) => {
+                            let url = machine.url;
+                            return await axios.get(url).then( async (indiv) => {
+                                info.push(indiv.data);
+                            });
+                        });
+                    };
+
+                    await sleep(300);
+
+                    info && info.map( (machine) => {
+                        let x = machine.item.url
+                        details[x] = null;
+                    });
+                    
+                    let urls = Object.keys(details)
+
+                    if (urls) {
+                        urls.map( async (url) => {
+                            return await axios.get(url).then( (info) => {
+                                details[url] = info
+                            });
+                        });
+                    };
+
+                    await sleep(300);
+
+
+                    setMachineData([info, details]);
+                    setPageData( {count: data.count, next: data.next, prev: data.previous} );
+
+                } catch (err) {
+                    setMachineData(null);
+                    console.log(err);
+                };
+            };
+
+            fetchList();
+        }, [ props.match.params.page ]
+    );
+
+    if (machineData && (getPage * 20 <= pageData.count)) {
+        desc = (
+            <p>
+                Machines are the representation of items that teach moves to Pokémon. They vary from 
+                version to version, so it is not certain that one specific TM or HM corresponds to a 
+                single Machine.
+            </p>
+        )
+        col = machineData[0] && machineData[0].map( (machine, index) => {
+            let ind = machine
+            let ind2 = machineData[1]
+
+            if (ind2[ind.item.url] && ind2[ind.item.url].data.sprites.default) {
+                img = <img alt = {ind.item.name + "Image"} src = {ind2[ind.item.url].data.sprites.default} />
+            } else {
+                img = <img className = 'notFound' alt = {ind.item.name + "Image"} src = {noImage} />
+            }
+
+            return (
+                <Col className = 'pokeCol' key = {ind.id}>
+                    <div className = 'pokeLink'>
+                        <Link to = {`/machines/${ind.id}`}>
+                            {img}
+                            <br />
+                            {ind.item.name}
+                            <br />
+                            <div className = 'sub'>
+                                ({ind.version_group.name})
+                            </div>
+                        </Link>
+                    </div>
+                </Col>
+            );
+        });
+    };
+
+    // NotFound Component if no PokeData
+    if (machineData === null || getPage > Math.floor(pageData.count / 20)) {
+        return (
+            <NotFound/>
+        ); 
+    };
+
+    // Pagination Helper Function
+    const pageNum = (bool) => {
+        let page = 0
+        if (bool) {
+            page = getPage + 1;
+        } else {
+            page = getPage - 1;
+        };
+        setGetPage(page);
+    };
+        
+    // Pagination Component
+    if (getPage <= Math.floor(pageData.count / 20)) {
+        page = <Page pageNum = {[pageNum, getPage, pageData.count, 'machines']} />
+    } else {
+        page = null
+    };
+
     return (
-        <div>
-            <h1> MACHINES </h1>
-        </div>
+        <Container>
+            <br />
+            <br />
+            <Row>
+                {desc}
+            </Row>
+            <Row className = 'page' sm = {3} md = {3} lg = {5}>
+                {col} 
+            </Row>
+            <br />
+            <Row className = 'page'> 
+                {page}
+            </Row>
+        </Container>
     );
 };
 
